c985bb602bb4a2834941508fd8c6a5cda06c9e1d
sai <saisreenath.mandaloju@gmail.com>
2024-09-28 23:09:48 +0530

    added implementation of course and student entities

--- src/main/java/com/example/university/service/CourseJpaService.java
+++ src/main/java/com/example/university/service/CourseJpaService.java
@@ -3,21 +3,38 @@ package com.example.university.service;
 import com.example.university.model.Course;
 import com.example.university.model.Professor;
 import com.example.university.model.Student;
+import com.example.university.repository.CourseJpaRepository;
 import com.example.university.repository.CourseRepository;
+import com.example.university.repository.ProfessorJpaRepository;
+import com.example.university.repository.StudentJpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class CourseJpaService implements CourseRepository {
+    @Autowired
+    private CourseJpaRepository courseJpaRepository;
+
+    @Autowired
+    private StudentJpaRepository studentJpaRepository;
+
+    @Autowired
+    private ProfessorJpaRepository professorJpaRepository;
+
     @Override
     public List<Course> getCourses() {
-        return List.of();
+        return courseJpaRepository.findAll();
     }
 
     @Override
     public Course getCourseById(int id) {
-        return null;
+        return courseJpaRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+        );
     }
 
     @Override
@@ -37,11 +54,17 @@ public class CourseJpaService implements CourseRepository {
 
     @Override
     public List<Student> getCourseStudents(int courseId) {
-        return List.of();
+        Course course = courseJpaRepository.findById(courseId).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+        );
+        return course.getStudents();
     }
 
     @Override
     public Professor getCourseProfessor(int courseId) {
-        return null;
+        Course course = courseJpaRepository.findById(courseId).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+        );
+        return course.getProfessor();
     }
 }
--- src/main/java/com/example/university/service/StudentJpaService.java
+++ src/main/java/com/example/university/service/StudentJpaService.java
@@ -1,23 +1,48 @@
 package com.example.university.service;
 
+import com.example.university.model.Course;
 import com.example.university.model.Student;
+import com.example.university.repository.CourseJpaRepository;
+import com.example.university.repository.StudentJpaRepository;
 import com.example.university.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StudentJpaService implements StudentRepository {
+    @Autowired
+    private StudentJpaRepository studentJpaRepository;
+
+    @Autowired
+    private CourseJpaRepository courseJpaRepository;
+
     @Override
     public List<Student> getStudents() {
-        return List.of();
+        return studentJpaRepository.findAll();
     }
 
     @Override
     public Student getStudentById(int id) {
-        return null;
+        return studentJpaRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+        );
     }
 
     @Override
     public Student addStudent(Student student) {
-        return null;
+        List<Integer> courseIds = new ArrayList<>();
+        for (Course c: student.getCourses()) {
+            courseIds.add(c.getCourseId());
+        }
+
+        List<Course> courses = courseJpaRepository.findAllById(courseIds);
+        if (courses.size() != courseIds.size()) throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+
+        student.setCourses(courses);
+
+        return studentJpaRepository.save(student);
     }
 }
